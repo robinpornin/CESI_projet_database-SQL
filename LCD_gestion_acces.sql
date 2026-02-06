6c9737ed6f1be9c9a188aac7f8448d17c1f9128f
Robin Pornin <porninrobin2007@gmail.com>
2026-02-06 11:40:32 +0100

    Update LCD_gestion_acces.sql

--- LCD_gestion_acces.sql
+++ LCD_gestion_acces.sql
@@ -1,7 +1,7 @@
 create user if not exists 'admin'@'localhost' identified by 'admin_mdp' ;
-grant all privileges on bdd_grp3.* to 'admin'@'localhost' ;
+grant all privileges on bdd_robin.* to 'admin'@'localhost' ;
 
 create user if not exists 'user'@'localhost' identified by 'user_mdp' ;
-grant select on bdd_grp3.* to 'user'@'localhost' ;
+grant select on bdd_robin.* to 'user'@'localhost' ;
 
 flush privileges ;
